80badca4ea28cdecc84414d198d04e7ffaf5d94a
sarvesh <sarveshbhosale307@gmail.com>
2024-06-14 01:38:22 +0530

    Comments addition

--- src/main/java/Two_Pointers/KthElementOf2sortedlists.java
+++ src/main/java/Two_Pointers/KthElementOf2sortedlists.java
@@ -1,5 +1,13 @@
 package Two_Pointers;
 
+//Given two sorted arrays A and B, and another value k, print the kth element of the resultant sorted array.
+//
+//Example
+//A: [1, 2, 3, 4, 5, 6]
+//B: [3, 4, 4, 5, 6, 6]
+//Result: [1, 2, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6]
+//3rd element in the array is 3.
+//6th element in the array is 4.
 public class KthElementOf2sortedlists {
     public static void main(String[] args) {
         int[] arr1 = new int[]{2, 3, 6, 7, 9};
@@ -29,6 +37,8 @@ public class KthElementOf2sortedlists {
                 right++;
             }
         }
+        //At this stage, any one of the pointer left or right will point to the end index of array,
+        // hence we can execute below while loops in any order either left or right first
         while (left < arr1.length) {
             counter++;
             if (counter == k)
